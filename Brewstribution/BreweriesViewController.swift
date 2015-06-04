0425b0d0fe264ca96a170653202ede7940aa3b46
snadeau <snadeau1@gmail.com>
2015-06-03 21:38:34 -0500

    Initial Commit

---
+++ Brewstribution/BreweriesViewController.swift
@@ -0,0 +1,74 @@
+//
+//  BreweriesViewController.swift
+//  Brewstribution
+//
+//  Created by Scott Nadeau on 6/3/15.
+//  Copyright (c) 2015 Scott Nadeau. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class BreweriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var breweriesTableView: UITableView!
+    
+    var state: PFObject?
+    var breweries: [PFObject]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        breweriesTableView.hidden = true
+        breweriesTableView.delegate = self
+        breweriesTableView.dataSource = self
+        
+        if state != nil {
+            self.navigationItem.title? = state!["name"] as! String
+            activityIndicator.startAnimating()
+            let distributionsQuery = PFQuery(className: "Distribution")
+            distributionsQuery.whereKey("state", equalTo: state!)
+            distributionsQuery.includeKey("brewery")
+            distributionsQuery.orderByAscending("brewery")
+            distributionsQuery.findObjectsInBackgroundWithBlock() {(objects, error) -> Void in
+                if (error != nil) {
+                    println("error finding distributions \(error)")
+                } else {
+                    let distributions = objects as! [PFObject]
+                    self.breweries = [PFObject]()
+                    for distribution in distributions {
+                        let brewery = distribution["brewery"] as! PFObject
+                        self.breweries?.append(brewery)
+                    }
+                    self.activityIndicator.stopAnimating()
+                    self.breweriesTableView.reloadData()
+                    self.breweriesTableView.hidden = false
+                    //Debugging purposes
+                    for brewery in self.breweries! {
+                        let breweryName = brewery["name"] as! String
+                        println(breweryName)
+                    }
+                }
+            }
+        }
+        
+        //Debugging purposes
+        if let stateName = state?["name"] as? String {
+            println("\(stateName) selected")
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (breweries != nil) {
+            return breweries!.count
+        } else {return 0}
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("breweryCell") as! UITableViewCell
+        let brewery = breweries![indexPath.row] as PFObject
+        let breweryName = brewery["name"] as! String
+        cell.textLabel?.text = breweryName
+        return cell
+    }
+
+}
---
+++ Brewstribution/BrewstributionViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Brewstribution
+//
+//  Created by Scott Nadeau on 6/3/15.
+//  Copyright (c) 2015 Scott Nadeau. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class BrewstributionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    var states: [PFObject]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        statePicker.delegate = self
+        statePicker.dataSource = self
+        
+        let stateQuery = PFQuery(className: "State")
+        stateQuery.orderByAscending("name")
+        stateQuery.findObjectsInBackgroundWithBlock() { (objects, error) -> Void in
+            if error != nil {
+                println("error finding states \(error)")
+            }
+            else {
+                self.states = objects as! [PFObject]?
+                self.statePicker.reloadAllComponents()
+            }
+        }
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if identifier == "showBreweries" {
+                let dvc = segue.destinationViewController as! BreweriesViewController
+                if states != nil {
+                    let state = states![statePicker.selectedRowInComponent(0)]
+                    dvc.state = state
+                    
+                    //self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Plain, target: nil, action: nil)
+                }
+                
+            }
+        }
+    }
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if states == nil {
+            println("states is nil")
+            return 0
+        } else {
+            return states!.count
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return states![row]["name"] as! String
+    }
+
+
+}
+
